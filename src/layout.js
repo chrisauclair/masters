0f63b465e792539f1de642bf69a6d01d94a87457
Chris Auclair <chris.auclair@gmail.com>
2015-06-25 13:10:43 -0400

    No refresh on local routes

--- src/layout.js
+++ src/layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavHelper from './components/nav-helper'
 
 export default React.createClass({
 	render () {
 		return (
-			<div>
+			<NavHelper>
 			  <nav className='top-nav top-nav-light cf' role='navigation'>
 			    <input id='menu-toggle' className='menu-toggle' type='checkbox'/>
 			    <label htmlFor='menu-toggle'>Menu</label>
@@ -16,7 +17,7 @@ export default React.createClass({
 			  <div className='container'>
 			  	{this.props.children}
 			  </div>
-			</div>
+			</NavHelper>
 		)
 	}
 })
